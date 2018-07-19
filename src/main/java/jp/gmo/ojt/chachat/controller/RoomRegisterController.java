52f97a2f8f67d9de44088d21ce2a9cbf4915f680
gmo-hashimoto <ge-hahsimoto@gmo-pg.com>
2018-07-19 16:28:14 +0900

    URL作成フォームを作成、今のところ入力した部屋名をそのまま貼り付けたURLを張り出し

--- src/main/java/jp/gmo/ojt/chachat/controller/RoomRegisterController.java
+++ src/main/java/jp/gmo/ojt/chachat/controller/RoomRegisterController.java
@@ -1,23 +1,42 @@
 package jp.gmo.ojt.chachat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
-import jp.gmo.ojt.chachat.bean.RoomRegistResult;
+//import jp.gmo.ojt.chachat.bean.RoomRegistResult;
+import jp.gmo.ojt.chachat.form.RegistForm;
 
 @Controller
 @RequestMapping("/")
 public class RoomRegisterController {
-	@RequestMapping(method=RequestMethod.GET)
-	public String index() {
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
 		return "room_register.html";
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
-	@ResponseBody
-	public RoomRegistResult register() {
-		return new RoomRegistResult("12345");
+	@ModelAttribute
+	RegistForm setUpForm() {
+		RegistForm form = new RegistForm();
+
+		return form;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	// @ResponseBody
+	public String register(@Validated RegistForm form, BindingResult bindingResult, Model model) {
+		// aaルーム名がからのときエラー文
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("valid_error", "1");
+			return "room_register.html";
+		}
+		model.addAttribute("valid_error", "0");
+
+		return "room_register.html";
 	}
 }
