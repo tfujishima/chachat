78b0784191d3290ed2687d3253cac23053232194
gmo-hashimoto <ge-hahsimoto@gmo-pg.com>
2018-07-20 13:20:22 +0900

    デバッグ用printlnを残していたので削除

--- src/main/java/jp/gmo/ojt/chachat/domain/service/RoomService.java
+++ src/main/java/jp/gmo/ojt/chachat/domain/service/RoomService.java
@@ -17,16 +17,10 @@ public class RoomService {
 	public void registRoom(Room room) {
 		String roomNameTmp = room.getRoomName();
 		String roomId = RoomUtility.makeRoomId(roomNameTmp);	
-		System.out.println("#####" + roomId);
 		while (!roomRepository.findById(roomId).equals(Optional.empty())) {
-			System.out.println("#####"+roomRepository.findById(roomId).equals(Optional.empty()));
-			System.out.println("#####" + roomNameTmp);
 			roomNameTmp = roomNameTmp + "pikachu";
 			roomId = RoomUtility.makeRoomId(roomNameTmp);
-			System.out.println("#####" + roomNameTmp);
-			System.out.println("#####" + roomId);
 		}
-		System.out.println(roomRepository.findById(roomId).equals(Optional.empty()));
 		room.setRoomId(roomId);
 		roomRepository.save(room);
 	}
