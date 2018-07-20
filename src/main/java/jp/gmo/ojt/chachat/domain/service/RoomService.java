aa2a050821083bdcd696b10006af35f89dcc5af4
gmo-hashimoto <ge-hahsimoto@gmo-pg.com>
2018-07-20 16:42:44 +0900

    無いIDは404へ飛ばす

--- src/main/java/jp/gmo/ojt/chachat/domain/service/RoomService.java
+++ src/main/java/jp/gmo/ojt/chachat/domain/service/RoomService.java
@@ -24,4 +24,12 @@ public class RoomService {
 		room.setRoomId(roomId);
 		roomRepository.save(room);
 	}
+	
+	public boolean roomIdisInDB(String roomId) {
+		System.out.println("result::::::::" + roomRepository.findById(roomId));
+		boolean check = !(roomRepository.findById(roomId).equals(Optional.empty())); 
+		System.out.println(check);
+		return check;
+		
+	}
 }
