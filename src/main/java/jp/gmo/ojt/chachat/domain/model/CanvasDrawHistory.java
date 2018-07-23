3e195fc588cdf31579ce36b14eb7c5d3b9904262
Tomoaki FUJISHIMA <tomoaki-fujishima@gmo.jp>
2018-07-24 00:48:38 +0900

    アノテーションミスの修正とgetter,setterの追加

--- src/main/java/jp/gmo/ojt/chachat/domain/model/CanvasDrawHistory.java
+++ src/main/java/jp/gmo/ojt/chachat/domain/model/CanvasDrawHistory.java
@@ -21,7 +21,7 @@ public class CanvasDrawHistory implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	@NotBlank
+    @NotNull
 	@Embedded
     private CanvasIdentity canvasIdentity;
 	@NotBlank
@@ -44,6 +44,77 @@ public class CanvasDrawHistory implements Serializable{
     @NotNull
     private Date createdAt;
 
+    
+    public CanvasIdentity getCanvasIdentity() {
+    	return this.canvasIdentity;
+    }
+    public void setCanvasIdentity(CanvasIdentity canvasIdentity) {
+    	this.canvasIdentity = canvasIdentity;
+    }
+    
+    
+	public String getRoomId() {
+		return this.canvasIdentity.getRoomId();
+	}
+	public void setRoomId(String roomId) {
+		this.canvasIdentity.setRoomId(roomId);
+	}
+	
+	public Integer getCanvasId() {
+		return this.canvasIdentity.getCanvasId();
+	}
+	public void setCanvasId(Integer canvasId) {
+		this.canvasIdentity.setCanvasId(canvasId);
+	}
+	
+	public String  getTargetId() {
+		return this.targetId;
+	}
+	public void setTargetId(String targetId) {
+		this.targetId = targetId;
+	}
+	public String  getMode() {
+		return this.mode;
+	}
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+	public String  getColor() {
+		return this.color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public Integer getLineWidth() {
+		return this.lineWidth;
+	}
+	public void setLineWidth(Integer lineWidth) {
+		this.lineWidth = lineWidth;
+	}
+	public Integer getFromX() {
+		return this.fromX;
+	}
+	public void setFromX(Integer fromX) {
+		this.fromX = fromX;
+	}
+	public Integer getFromY() {
+		return this.fromY;
+	}
+	public void setFromY(Integer fromY) {
+		this.fromY = fromY;
+	}public Integer getToX() {
+		return this.toX;
+	}
+	public void setToX(Integer toX) {
+		this.toX = toX;
+	}
+	public Integer getToY() {
+		return this.toY;
+	}
+	public void setToY(Integer toY) {
+		this.toY = toY;
+	}
+    
 	@PrePersist
 	protected void onCreate() {
 	  this.createdAt = new Date();
