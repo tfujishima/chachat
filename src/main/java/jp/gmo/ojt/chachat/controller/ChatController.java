546ff90d3941a8a979ad4f854fa5af96b30e8ff7
Tomoaki FUJISHIMA <tomoaki-fujishima@gmo.jp>
2018-07-25 21:20:45 +0900

    チャットデータの履歴を返すように

--- src/main/java/jp/gmo/ojt/chachat/controller/ChatController.java
+++ src/main/java/jp/gmo/ojt/chachat/controller/ChatController.java
@@ -1,10 +1,16 @@
 package jp.gmo.ojt.chachat.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jp.gmo.ojt.chachat.domain.model.ChatHistory;
 import jp.gmo.ojt.chachat.domain.service.ChatHistoryService;
@@ -20,6 +26,11 @@ public class ChatController {
 		chatHistory.setRoomId(roomId);
 		chatHistoryService.saveChatHistory(chatHistory);
 		return chatHistory;
+	}
+	@ResponseBody
+	@RequestMapping(value = "/rooms/{roomId}/chat/", method = RequestMethod.GET)
+	public List<ChatHistory> index(@PathVariable("roomId") String roomId) {
+		return chatHistoryService.getChatHistories(roomId);
 	}
 }
 
